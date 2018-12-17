0ecc625d4dfe69dd250b8afef1474475314edef3
olaborde <oml007@live.com>
2018-12-16 23:22:41 -0500

    passing data thru GET POST and URL segment

--- hello-spring/src/main/java/com/example/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/com/example/hellospring/controllers/HelloController.java
@@ -2,8 +2,7 @@ package com.example.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,5 +24,30 @@ public class HelloController {
     @ResponseBody
     public String goodbye(){
         return "See Ya";
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm(){
+        String html ="<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' /> ";
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+
+        return "Whats up, " + name + "!";
+
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
+        return "Hello, " + name;
     }
 }
